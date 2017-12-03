a049e8fa6b05248b11c5e9c85ea15d6911522847
Kunal Shah <kunal8@gmail.com>
2017-12-03 14:19:55 -0500

    Start V2 react redesign

---
+++ README.md
@@ -0,0 +1,21 @@
+#React Branch
+##Todo
+###Structure
+- [ ] Setup react structure
+- [ ] webpack to public dir
+- [ ] Add timestamp when game is created by host
+- [ ] Cloud func to remove old games based on timestamp
+
+### Style
+- [ ] Create card object style
+- [ ] Button object style
+- [ ] React Pages with history
+
+### Features
+- [ ] Remove player option
+- [ ] If player name already exists check
+- [ ] Play again within same game room
+- [ ] randomize "host/reader"
+- [ ] Go back to read last card
+- [ ] points cary over to next game ? 
+
--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@ var functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
+'use strict';
+
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
@@ -17,3 +19,42 @@ exports.countWords = functions.database.ref('/Words/').onWrite(event => {
 exports.countThings = functions.database.ref('/Things/').onWrite(event => {
   return event.data.ref.child('count').set(event.data.numChildren());
 });
+
+// ---------------------------------------------------
+// Structure of Database
+// /functions-project-12345
+//     /path
+//         /to
+//             /items
+//                 item1: {
+//                     timestamp: 1497911193083
+//                 },
+//                 item2: {
+//                     timestamp: 1597911193083                    
+//                 }
+//                 ...
+// ---------------------------------------------------
+
+// // Cut off time. Child nodes older than this will be deleted.
+// const CUT_OFF_TIME = 24 * 60 * 60 * 1000; // 24 Hours in milliseconds.
+
+// /**
+//  * This database triggered function will check for child nodes that are older than the
+//  * cut-off time. Each child needs to have a `timestamp` attribute.
+//  */
+// exports.deleteOldItems = functions.database.ref('/path/to/items/{pushId}')
+//     .onWrite(event => {
+//       const ref = event.data.ref.parent; // reference to the items
+//       const now = Date.now();
+//       const cutoff = now - CUT_OFF_TIME;
+//       const oldItemsQuery = ref.orderByChild('timestamp').endAt(cutoff);
+//       return oldItemsQuery.once('value').then(snapshot => {
+//         // create a map with all children that need to be removed
+//         const updates = {};
+//         snapshot.forEach(child => {
+//           updates[child.key] = null;
+//         });
+//         // execute all updates in one go and return the result to end the function
+//         return ref.update(updates);
+//       });
+//     });
